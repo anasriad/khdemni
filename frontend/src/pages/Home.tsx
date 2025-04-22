f0c8341d6ff3787a101bf908ee5c4a6acc4d49fd
Anas Riad <riadanas2000@gmail.com>
2025-04-22 22:39:24 +0200

    header style

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,19 @@
 import Header from "../components/Header";
-
-export default function Home(){
+import { motion } from 'framer-motion'
+export default function Home() {
     return <>
         <div>
-            <Header/>
+            <div>
+                <Header />
+            </div>
+            <div>
+                <motion.div>
+                    <img src="../assets/Background.png" className="" />
+                </motion.div>
+                <motion.div>
+                    
+                </motion.div>
+            </div>
         </div>
     </>
 }
