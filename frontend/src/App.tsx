7c7f75676a332d29f3d3f53db4b9f0490314fbf9
reda189 <redabenyaich1@gmail.com>
2025-04-24 00:21:45 +0100

    tutorial

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Login from "./pages/login";
+import Tutorials from "./pages/tutorials";
 
 function App() {
-
-
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Header />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/tutorials" element={<Tutorials />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
